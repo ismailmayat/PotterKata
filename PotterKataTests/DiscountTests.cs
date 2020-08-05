c6c9779d51e094b9421b627d12c9b26be1983e38
Ismail Mayat <ism@novicell.co.uk>
2020-08-05 15:42:11 +0100

    refactor and adding new test for mixed basket

--- PotterKataTests/DiscountTests.cs
+++ PotterKataTests/DiscountTests.cs
@@ -7,9 +7,9 @@ namespace PotterKataTests
     [TestFixture]
     public class DiscountTests
     {
-        private const decimal OneBookPrice = 8;
+        private const decimal OneBookPrice = 8.00M;
 
-        private Discounter sut = ObjectMother.Builder.Discounter();
+        private readonly Discounter sut = ObjectMother.Builder.Discounter();
         
         [Test]
         public void One_Book_Costs_Fixed_Price()
@@ -27,7 +27,7 @@ namespace PotterKataTests
 
             var books = ObjectMother.Builder.TwoUniqueBooks(OneBookPrice);
         
-            sut.Apply(books).Should().Be((decimal)15.20);
+            sut.Apply(books).Should().Be(15.20M);
         }
 
         [Test]
@@ -35,7 +35,7 @@ namespace PotterKataTests
         {
             var books = ObjectMother.Builder.ThreeUniqueBooks(OneBookPrice);
         
-            sut.Apply(books).Should().Be((decimal)21.60);
+            sut.Apply(books).Should().Be(21.60M);
         }
 
         [Test]
@@ -43,7 +43,7 @@ namespace PotterKataTests
         {
             var books = ObjectMother.Builder.FourUniqueBooks(OneBookPrice);
 
-            sut.Apply(books).Should().Be((decimal) 25.60);
+            sut.Apply(books).Should().Be( 25.60M);
         }
 
         [Test]
@@ -51,7 +51,15 @@ namespace PotterKataTests
         {
             var books = ObjectMother.Builder.FiveUniqueBooks(OneBookPrice);
 
-            sut.Apply(books).Should().Be((decimal) 30.00);
+            sut.Apply(books).Should().Be(30.00M);
+        }
+
+        [Test]
+        public void Three_Unique_Books_Gives_Ten_Per_Cent_Discount_Forth_Is_Full_Price()
+        {
+            decimal expectedTotal = 29.60M;
+            
         }
     }
+    
 }
