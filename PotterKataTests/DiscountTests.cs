d7f676f1718cb0e1c2dcfd95e49240f935ae49e3
Ismail Mayat <ism@novicell.co.uk>
2020-08-05 12:25:04 +0100

    initial working test to flesh out classes required

--- PotterKataTests/DiscountTests.cs
+++ PotterKataTests/DiscountTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace PotterKataTests
@@ -7,9 +9,44 @@ namespace PotterKataTests
     public class DiscountTests
     {
         [Test]
-        public void Test1()
+        public void One_Book_Costs_Fixed_Price()
         {
-            Assert.True(true);
+            decimal oneBookPrice = 8;
+            
+            var book = new Book(oneBookPrice);
+            
+            IEnumerable<Book> books =new List<Book>{book};
+            
+            var sut = new Discount();
+
+            sut.Calculate(books).Should().Be(oneBookPrice);
         }
+        
+    }
+
+    public class Discount
+    {
+        public decimal Calculate(IEnumerable<Book> books)
+        {
+            decimal total=0;
+
+            foreach (var book in books)
+            {
+                total += book.OneBookPrice;
+            }
+
+            return total;
+        }
+    }
+
+    public class Book
+    {
+
+        public decimal OneBookPrice { get; }
+        public Book(decimal oneBookPrice)
+        {
+            OneBookPrice = oneBookPrice;
+        }
+        
     }
 }
