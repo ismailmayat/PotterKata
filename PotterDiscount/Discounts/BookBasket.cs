6fe1e727fae5cca9b28866e9cd854eee3211fb6e
Ismail Mayat <ism@novicell.co.uk>
2020-08-10 15:45:23 +0100

    remove unused code

--- PotterDiscount/Discounts/BookBasket.cs
+++ PotterDiscount/Discounts/BookBasket.cs
@@ -13,14 +13,7 @@ namespace PotterDiscount.Discounts
         }
 
         public IEnumerable<Book> Books => _books;
-
-        public int Count()
-        {
-            var hashset = new HashSet<string>();
-
-            return Books.Count(book => hashset.Add(book.Isbn));
-        }
-
+        
         public bool HasDuplicates()
         {
             var hashset = new HashSet<string>();
--- PotterKataTests/BookBasketTests.cs
+++ PotterKataTests/BookBasketTests.cs
@@ -9,25 +9,7 @@ namespace PotterKataTests
     [TestFixture]
     public class BookBasketTests
     {
-        [Test]
-        [TestCase("11111",1)]
-        [TestCase("11111,22222",2)]
-        [TestCase("11111,22222,33333",3)]
-        [TestCase("11111,22222,33333,44444",4)]
-        [TestCase("11111,22222,33333,44444,55555",5)]
-        [TestCase("11111,11111,33333,44444,55555",4)]
-        [TestCase("55555,55555,55555,55555,55555",1)]
-        public void For_Given_No_Unique_Should_Match(string isbns, int expectedCount)
-        {
-            var bookIsbns = isbns.Split(',');
-            
-            var books = bookIsbns.Select(isbn => new Book(8, isbn)).ToList();
-            
-            var sut = new BookBasket(books);
-
-            sut.Count().Should().Be(expectedCount);
-        }
-
+        
         [Test]
         public void List_With_Duplicates_Should_Be_True()
         {
