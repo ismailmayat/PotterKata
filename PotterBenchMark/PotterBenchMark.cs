9242b45759479bdb84125cb26b19b3c22aa6d641
Ismail Mayat <ism@novicell.co.uk>
2020-08-10 15:13:00 +0100

    working benchmark

--- PotterBenchMark/PotterBenchMark.cs
+++ PotterBenchMark/PotterBenchMark.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Order;
+using PotterDiscount;
 
 namespace PotterBenchMark
 {
@@ -8,9 +10,31 @@ namespace PotterBenchMark
     [RankColumn()]
     public class PotterBenchMark
     {
+        private BookSetBuilder _bookSetBuilder;
+        private readonly List<Book> _oneThousandBooks = new List<Book>();
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _bookSetBuilder = new BookSetBuilder(5);
+            CreateOneThousandBooksFiveOfEach();
+        }
+
+        private void CreateOneThousandBooksFiveOfEach()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    _oneThousandBooks.Add(new Book(8M,i.ToString()+j.ToString()));
+                }
+            }
+        }
+        
+        [Benchmark]
         public void PotterBasketOneThousandBooks()
         {
-            
+            _bookSetBuilder.Build(_oneThousandBooks);
         }
     }
 }
--- PotterBenchMark/Program.cs
+++ PotterBenchMark/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace PotterBenchMark
+﻿using BenchmarkDotNet.Running;
+
+namespace PotterBenchMark
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            BenchmarkRunner.Run<PotterBenchMark>();
         }
     }
 }
